cfc8d57e74b6c747d56895cb0b0b1ba8f3d8e310
MARIA FERNANDA FERNANDA <102367409+MariFer2207@users.noreply.github.com>
2022-04-10 02:43:29 +0000

    Primeros movimientos a la izquierda personaje_espacio y personaje_meta.

--- sokoban.py
+++ sokoban.py
@@ -34,9 +34,11 @@ muñeco_columna = 5
 def imprimirMapa(self):
     for fila in self.mapa:
         print(fila)
+"""Imprime el mapa completo"""
 
 def moverDerecha(self):
-        
+     """Controla los movimientos a la derecha"""
+
 #00 - Personaje, camino -> [2,3] -> [3,2]
     if self.mapa[self.muneco_fila][self.muneco_columna] == 2 and self.mapa[self.muneco_fila][self.muneco_columna + 1] == 3:
         self.mapa[self.muneco_fila][self.muneco_columna] = 3
@@ -117,6 +119,19 @@ def moverDerecha(self):
         self.mapa[self.muneco_fila][self.muneco_columna +2] = 6
         self.muneco_columna += 1
 
+def moverIzquierda(self):
+"""Controla los movimiento a la izquierda"""
+
+#12 - Personaje, espacio -> [3,2] -> [2,3]
+    if self.mapa[self.muneco_fila][self.muneco_columna] == 3 and self.mapa[self.muneco_fila][self.muneco_columna - 1] == 2:
+        self.mapa[self.muneco_fila][self.muneco_columna] = 2
+        self.mapa[self.muneco_fila][self.muneco_columna - 1] = 3
+        self.muneco_columna -= 1
+#13 - Personaje, meta -> [4,2] -> [5,3]
+    if self.mapa[self.muneco_fila][self.muneco_columna] == 4 and self.mapa[self.muneco_fila][self.muneco_columna + 1] == 2:
+        self.mapa[self.muneco_fila][self.muneco_columna] = 5
+        self.mapa[self.muneco_fila][self.muneco_columna + 1] = 3
+        self.muneco_columna += 1
         
 
 def jugar(self):
